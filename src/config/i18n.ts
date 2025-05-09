b27f910edb07bbe551b09e446aba1938ac120965
Ahmed Khaled <ahmedkhaled4d@gmail.com>
2025-05-09 14:36:20 +0300

    i18n

--- src/config/i18n.ts
+++ src/config/i18n.ts
@@ -1,19 +1,12 @@
 import i18n from 'i18next'
 import { initReactI18next } from 'react-i18next'
+// Translations
+import arTranslations from '../i18n/ar.json'
+import enTranslations from '../i18n/en.json'
 
 const resources = {
-  en: {
-    translation: {
-      welcome: 'Welcome',
-      dashboard: 'Dashboard',
-    },
-  },
-  ar: {
-    translation: {
-      welcome: 'مرحبًا',
-      dashboard: 'لوحة التحكم',
-    },
-  },
+  en: { translation: enTranslations },
+  ar: { translation: arTranslations },
 }
 
 i18n.use(initReactI18next).init({
